5ccb6a8fc644f5905c4651cbe05b117298f0a56f
watanabeyu1218 <k23145kk@aitech.ac.jp>
2025-01-16 13:17:58 +0900

    不要
    いらない

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ def get_schedule():
     #徒歩やバス移動は「分単位」、時刻は "HH:MM:SS"
     #現在時刻は13:00:00という設定　固定値から変更が必要
     response = {
-        "currentTime": "13:00:00",
-        "timeToBusStop": 300,  # 徒歩5分
-        "busDepartureTimes": ["13:15:00", "13:30:00", "13:45:00"],
-        "busTravelTime": 360,  # バス移動6分
-        "trainDepartureTimes": ["14:00:00", "14:30:00"]
+        "currentTime": "13:27:00",
+        "time_to_bus_stop": 300,  # 徒歩5分
+        "bus_departure_times": ["13:15:00", "13:20:00", "13:25:00", "13:30:00", "13:35:00", "13:40:00", "13:45:00", "13:50:00", "13:55:00", "14:00:00"],
+        "bus_travel_time": 360,  # バス移動6分
+        "train_departure_time": ["13:40:00", "14:00:00", "14:20:00", "14:40:00", "15:00:00"]
     }
     return jsonify(response)
 
